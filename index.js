b879e234aa52adf116309bece5f09bd539062f05
bananaroti <gh3298@gmail.com>
2021-12-02 17:34:20 +0900

    JSX > convert babel > ReactJS
    JSX문법으로 작성한 파일을 babel에서 변환하여 ReactJS문법으로 변환시켜준다.
    
    .js파일에서 JSX문법에 맞게 작성하고 .html파일에서
    script 태그작성하고 babel 홈페이지에서 링크를 등록하고 JSX문법을 작성한곳에 text="text/babel"을 추가해주면 끝

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const root = document.getElementById("root");
-const h3 = React.createElement("h3", {id:"title", onMouseEnter:() => console.log("mouse Enter!")}, "Hello, I'm a span!");
-const btn = React.createElement("button", {onClick:() => console.log("Clicked!")}, "Click me!");
-const container = React.createElement("div", null, [h3, btn]);
-ReactDOM.render(container,root);
+const title = (
+  <h3 id="title" onMouseEnter={() => console.log("onMoused!")}>
+    Hello, i'm title!
+  </h3>
+);
+// const h3 = React.createElement("h3", {id:"title", onMouseEnter:() => console.log("mouse Enter!")}, "Hello, I'm a span!");
+
+// const btn = React.createElement("button", {onClick:() => console.log("Clicked!")}, "Click me!");
+const button = (
+  <button
+    id="button"
+    style={{ backgroundColor: "tomato" }}
+    onClick={() => console.log("btnClick!")}
+  >
+    Click me!
+  </button>
+);
+const container = React.createElement("div", null, [title, button]);
+ReactDOM.render(container, root);
