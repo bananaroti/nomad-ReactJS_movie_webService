8e5247f339e2b9fc25abd4eb6352310126cb249a
bananaroti <gh3298@gmail.com>
2021-12-04 18:12:21 +0900

    udate js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const root = document.getElementById("root");
 
 function App() {
+  const [minutes, setminutes] = React.useState(0);
+  const onChange = (event) => {
+      setminutes(event.target.value);
+  }
   return(
   <div>
     <h1>Super Converter</h1>
-    <label for="minutes">Minutes</label>
-    <input id="minutes" placeholder="Minutes" type="number"></input>
-    <label for="hours">Hours</label>
-    <input id="hours" placeholder="Hours" type="number"></input>
+    <label htmlfor="minutes">Minutes</label>
+    <input id="minutes" placeholder="Minutes" type="number" onChange={onChange}></input>
+    <label htmlfor="hours">Hours</label>
+    <input id="hours" placeholder="Hours" type="number" value={Math.floor(minutes / 60)}></input>
   </div>
   )
 }
